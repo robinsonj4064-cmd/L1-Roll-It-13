a2ec8f87d2e22383152e54a79cf11c39c8c3f864
SlimRidinghood <robinsonj4064@masseyhigh.school.nz>
2026-02-11 12:11:43 +1300

    yes / no checker, does not loop

---
+++ C_01_yes_no_basic.py
@@ -0,0 +1,10 @@
+want_instructions =input("Do you want to see the instructions").lower()
+
+#check the user say yes / no
+if want_instructions == "yes" or want_instructions == "y":
+    print("you said yes")
+elif want_instructions == "no" or want_instructions == "n":
+    print("you said no")
+else:
+    print("please enter yes / no")
+
