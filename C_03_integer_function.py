2be857899977757667ea9076a4a9b9f71d6ca648
SlimRidinghood <robinsonj4064@masseyhigh.school.nz>
2026-02-17 09:48:06 +1300

    Working number checker function

---
+++ C_03_integer_function.py
@@ -0,0 +1,24 @@
+from C_03_integer_checker_v1 import game_goal
+
+
+def int_check():
+    """Checks users enetr on integer more than / equal to 13"""
+
+    error = "Please enter an integer more than / equal to 13."
+
+    while True:
+        try:
+            response = int(input("What is the game goal? "))
+
+            if response < 13:
+                print(error)
+            else:
+                return response
+
+        except ValueError:
+            print(error)
+
+
+# main routine starts here
+game_goal = int_check()
+print(game_goal)
