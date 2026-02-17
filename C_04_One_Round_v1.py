053db1ccadec2295a30c19953ad0cb9dccc31fee
SlimRidinghood <robinsonj4064@masseyhigh.school.nz>
2026-02-18 10:33:04 +1300

    comp and user are getting dif points

---
+++ C_04_One_Round_v1.py
@@ -0,0 +1,32 @@
+import random
+
+# Intialise rounds points
+user_points = 0
+comp_points = 0
+double_user = "no"
+
+# Roll the dice for the user and note if they get a double
+user_one = random.randint(1,6)
+user_two = random.randint(1,6)
+
+if user_one == user_two:
+    double_user = "yes"
+
+
+# roll the dice for the computer
+comp_one = random.randint(1,6)
+comp_two = random.randint(1,6)
+
+# Update the user / computer points
+user_points += user_one + user_two
+comp_points += comp_one + comp_two
+
+# Output the result
+# Let the user know if they qualify for double points
+print("\nInitial Points")
+print(f"User     - Roll 1: {user_one} \t|  Roll: {user_two} \t | Total: {user_points}")
+print(f"Computer     - Roll 1: {comp_one} \t|  Roll: {comp_two} \t | Total: {comp_points}")
+
+# Let the user know if they qualify for double points
+if double_user == "yes":
+    print("Great news - if u win, you will earn double points")
